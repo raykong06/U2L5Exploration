01ce6a9b3b47a9158a2c5a39845c2c66ea27cab0
BT_4N2_24 <rkong2588@bths.edu>
2022-10-03 12:28:19 -0400

    Initial commit

---
+++ src/U2L5Runner.java
@@ -0,0 +1,9 @@
+public class U2L5Runner {
+    public static void main(String[] args) {
+        ShoppingListItem item = null;
+        String s = item.toString();
+        item = new ShoppingListItem("apple", 0.50, 10);
+        double total = item.totalCost();
+
+    }
+}
